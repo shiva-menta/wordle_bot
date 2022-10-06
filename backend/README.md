d7f6e57754da383eb0b1df362f83452110b53db4
Shiva Menta <shiva.123.menta@gmail.com>
2022-10-06 04:22:27 -0400

    Code First Iteration of Filter Word Operations

--- backend/README.md
+++ backend/README.md
@@ -1,27 +1,58 @@
 # wordle-bot
-Automatic Wordle solver bot that chooses the optimal guess at every opportunity.
+Automatic Wordle solver bot that guesses a mystery 5-letter word in optimal 
+time.
 
 Problem Statement:
-Every day, a random five-letter word is chosen and the user is given 6 guesses.
-The program aims to choose the most optimal word possible at every guess,
-maximizing letters used and avoiding reguesses of existing letters. Each
-guess will need to take into account which letters have been guessed, which
-letters have a confirmed position, which letters are in the word but in an
-incorrect position.
+Wordle is an online game in which users try to guess a five letter word. A new 
+word is uploaded daily.
 
-We have a csv file with all five-letter words to be considered. Data must be 
-loaded in a way such that each guess can quickly evaluate the word with the
-most likely answer (most common letters based on available positions).
+A user is given six guesses per round. After each guess, a user is told how
+much information they have correct. A yellow character indicates that the
+letter is in the word but in the wrong position. A green character indicates
+that the letter is in the word AND is in the correct position. A gray character
+indicates that the letter is not in the word.
 
-Steps To Take:
-- Filtering array of words to exclude words with already guessed letters.
+A user's goal is to guess the correct word in the fewest number of attempts.
+Wordle also has two modes: normal and hard mode. In Hard Mode, all information
+sourced previously has to be used in subsequent guesses (e.g. yellow and
+green letters). We will be 'playing' in Wordle Hard Mode.
 
-- Filtering array of words to include words with 'yellow' letters.
+Algorithm Logic:
+Taking inspiration from 3Blue1Brown, our algorithm will depend on information
+theory.
 
-- Filtering array of words to include words with 'green' letters in specific positions.
+In brief terms, given a pool of existing words, the optimal guess is the one
+that contains the highest expected value of information. The expected value
+is calculated by multiplying two terms: the probability of getting a certain
+word template multiplied by the amount of information that we can source from
+it. We'll dive a bit deeper into each term below.
 
-- Collect words that have highest frequency in each position and choose the most 
-- optimal word in a subset. (get_optimal_word(arr))
+1: Word Template – There are a total of 3^5 different word templates we can
+have in Wordle, each composed of different combinations of gray, yellow, and
+green letters. If we calculate the expected value for each of these word
+template possibilities, we'll be able to assess the strength of each word
+guess.
 
-Potential Data Storage + SearchStrategies:
-Ideas: 2D Arrays, HashMaps, Tries
+2: Amount of Information (bits) – We'll quantify the amount of information
+we're getting using bits. A bit of information corresponds to the power of 2
+associated with the number of words we're cutting down to for future guesses.
+For example, if my original word pool has 4 words, and we cut it down to 2
+words with our guess, we can say that our guess had 1 bit of information (cut
+down by a factor of 2^1). Another way of quantifying a bit is considering how
+many times our available word pool is cut down by half.
+
+Now that we've explained both terms, we'll simply use our Python scripts
+to calculate the optimal guess at each round until a guess is made. 
+
+To make the bot perform even better, I added an endgame solution parameter of
+N = 3. Essentially, once the word pool is N = 2,3, the Wordle bot will choose
+the word that has the highest frequency according to word usage data. The pool
+of Wordle answers is relatively small (~2000), and they're generally the most
+frequently used / common five-letter words. For this reason, once we reach a
+small enough word pool, we'll select our next guess using this method.
+
+With this, our logic is complete. Hope you enjoy the project :)
+
+Data Sources:
+I sourced the list of all possible words using WordsAPI. Word frequency
+data was collected using Wolfram's WordFrequency Data function.
--- backend/src/wordle_solver.py
+++ backend/src/wordle_solver.py
@@ -2,57 +2,85 @@ import requests
 import string
 import csv
 import os
+import math
 from datetime import date
 from collections import Counter
 
 URL = "https://www.nytimes.com/games-assets/v2/wordle.1b4655b170d30c964441b708a4e22b3e617499a1.js"
 page = requests.get(URL)
 start_date = date(2021, 6, 19)
+endgame_threshold = 3
 
-def filter_words(guess, template, word_list):
-    template_array = list(template)
-    guess_array = list(guess)
-    green_indices = []
-    contains_list = []
-    cleaned_words_list = []
 
-    # Reading through template.
-    for i in range(5):
-        if template_array[i] == "y":
-            contains_list.append(str(guess_array[i]))
-        if template_array[i] == "g":
-            green_indices.append(i)
 
-    for word in word_list:
-        word_bool = True
+def expected_value(denom, numer):
+    return (numer / denom) * -1 * math.log((numer / denom), 2)
 
-        # Filters out words that don't match green letters.
-        if green_indices :
-            for index in green_indices:
-                if guess[int(index)] == word[int(index)] :
-                    word_bool = word_bool and True
-                else :
-                    word_bool = False
-                    break
-        
-        # Filters out words that don't contain yellow letters.
-        if word_bool and contains_list :
-            for char in contains_list:
-                if char in word:
-                    word_bool = word_bool and True
-                else :
-                    word_bool = word_bool and False
-        
-        # Adds word to return list.
-        if word_bool:
-            cleaned_words_list.append(word)
+def get_all_template_types():
+    template_type = ['x', 'y', 'g']
+    ret = []
 
-    print(cleaned_words_list)
+    for first in template_type:
+        for second in template_type:
+            for third in template_type:
+                for fourth in template_type:
+                    for fifth in template_type:
+                        ret.append(first+second+third+fourth+fifth)
+    
+    return ret
+
+def filter_words(word, template, word_arr):
+    return list(filter(lambda guess: word_matches_template(guess, word, template), word_arr))
+
+# checks that a given guess would be valid given a word template
+def word_matches_template(guess, word, template):
+    green_indices = set()
+    yel_stor = {}
+    gray_stor = set()
+
+    for count, letter in enumerate(template):
+        if letter == 'g':
+            green_indices.add(count)
+        elif letter == 'y':
+            yel_stor[word[count]] = yel_stor.get(word[count], []) + [count]
+        else:
+            gray_stor.add(word[count])
+    
+    if len(gray_stor) == 5:
+        return False
+
+    pot_yel_ind = set([0,1,2,3,4]).difference(green_indices)
+
+    if green_indices:
+        for index in green_indices:
+            if guess[index] != word[index]:
+                return False
+
+    if yel_stor:
+        for key, val in yel_stor.items():
+            act_ind = val
+
+            check_ind = pot_yel_ind.copy()
+            for ind in act_ind:
+                check_ind.remove(ind)
+            
+            for ind in check_ind:
+                if key == guess[ind] and act_ind:
+                    act_ind.pop()
+
+            if act_ind:
+                return False
+    
+    if gray_stor:
+        for ind in pot_yel_ind:
+            if guess[ind] in gray_stor:
+                return False
+
+    return True
             
 def get_answer_template(guess, answer):
     return_list = ['x'] * 5
     stor = Counter(answer)
-    
 
     for i in range(5):
         if guess[i] == answer[i]:
@@ -99,14 +127,41 @@ def get_todays_word():
 
     print(todays_word)
 
+def play_full_game(ans):
+    guess_arr = []
+    curr = None
 
+    # Read CSV Data into Array
+    word_list = None
 
+    while curr != ans and len(guess_arr) <= 6:
+        curr = get_best_guess(word_list)
+        guess_arr.append(curr)
 
-def source_todays_word():
-    
-    return
+        word_list = filter_words(curr, get_answer_template(curr, ans), word_list)
+
+    return guess_arr
+
+def find_best_guess(arr):
+    denom = len(arr)
+    best_arr = [0] * len(arr)
+    exp = 0
+
+    for index, word in range(len(arr)):
+        for temp in get_all_template_types():
+            exp += expected_value(denom, len(filter_words(word, temp, arr)))
+        
+        best_arr[index] = exp
+        exp = 0
+
+    return max(best_arr)
+
+    # for every word, use filter_words on every word template and calculate expected 
 
 
 # filter_words("crane", "xxyxg", ['alike','ranch','strre','saute','alive'])
-print(get_answer_template("shiee", "icert"))
+# print(get_answer_template("shiee", "icert"))
 # read_answer_array_into_csv(source_and_clean_wordle_text_dump())
+# get_todays_word()
+# print(word_matches_template('smart','stara','gyggx'))
+print(filter_words('eeeee','xxxxx',['shiva', 'hello', 'smart', 'group']))
